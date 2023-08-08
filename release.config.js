2fc6d27aa38372ede02b3d00e5e7c9388418404e
Tamal Das <tamalcodes@gmail.com>
2023-08-09 02:59:56 +0530

    fix: removing build commands

--- release.config.js
+++ release.config.js
@@ -6,7 +6,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/*.js.map"],
+        assets: ["release/*.js", "release/*.js.map"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
