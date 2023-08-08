c1c3754d107534c78bceebd60a0437f920329d77
Tamal Das <tamalcodes@gmail.com>
2023-08-09 01:51:25 +0530

    feat: updating the way mongoDB connects

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
-//This is a demo db , the original files are safe
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connectToMongo = () => {
-  mongoose.connect(process.env.MONGO_URI, () => {
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("CONNECTED TO MONGO DB DATABASE 🌐");
-  });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
 
 module.exports = connectToMongo;
